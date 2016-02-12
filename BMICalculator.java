7a95b767e0f5c8e705df9b4479776f247e9df20b
Obosa Osagie-Amayo <prosperosagieamayo@gmail.com>
2016-02-11 22:59:33 -0800

    Update BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -10,57 +10,53 @@ import java.util.Scanner;
 
 public class BMICalculator {
 
+	static Scanner in = new Scanner(System.in);
+	static double weight, heightFeet, heightInches, heightCalc, heightTotal,
+					bmi;
+
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int weight1, heightFeet, heightInches;
 		
 		System.out.printf("How much do you weigh?\nPounds: ");
-		weight1 = in.nextInt();
+		weight = in.nextInt();
 		
 		System.out.printf("\nHow tall are you?\nFeet: ");
 		heightFeet = in.nextInt();
 		System.out.print("Inches: ");
 		heightInches = in.nextInt();
-		double bmiResult = bmiCalc(weight1, heightToInches(heightFeet,
-				heightInches));
 		
-		System.out.printf("\nYour BMI is: %.2f\n", bmiResult);
-		if (bmiResult > 18.5 && bmiResult < 25.0)
+		BMICalculator.bmiCalc();
+
+		System.out.printf(
+				"BMI%23s\n" +
+				"---	      ------------\n" +
+				"<%.1f%20s\n" +
+				"%.1f-%.1f%18s\n" +
+				"%d-%.1f%17s\n" +
+				"%d-%.1f%12s\n" +
+				">%d%25s\n",
+				"Weight Range", 18.5,
+				"Underweight", 18.5, 24.9,
+				"Normal Weight", 25, 29.9,
+				"Overweight", 30, 39.9,
+				"Obese", 40,
+				"Morbidly Obese");
+
+	}
+	
+	static void bmiCalc() {
+		
+		//Height Conversion
+		heightCalc = heightFeet * 12 + heightInches;
+		heightTotal = heightCalc * heightCalc;
+		
+		//BMI Calculation
+		bmi = ((weight / heightTotal) * 703);
+	
+		System.out.printf("\nYour BMI is: %.2f\n", bmi);
+		if (bmi > 18.5 && bmi < 25.0)
 			System.out.print("You have a healthy BMI!\n\n");
 		else
 			System.out.print("You have an uhealthy BMI!\n\n");
-		
-		System.out.printf("BMI%23s\n" +
-				  "<%.1f%20s\n" +
-				  "%.1f-%.1f%18s\n" + 
-				  "%d-%.1f%17s\n" +
-				  "%d-%.1f%12s\n" +
-				  ">%d%25s\n",
-				  "Weight Range", 18.5,
-				  "Underweight", 18.5, 24.9,
-				  "Normal Weight", 25, 29.9,
-				  "Overweight", 30, 39.9,
-				  "Obese", 40,
-				  "Morbidly Obese");
-		
-		in.close();
-		
-	}
-	
-	//Height conversion to inches
-	public static int heightToInches(int feet, int inches) {
-		int totalInches = (feet * 12) + inches;
-		int totalInches2 = totalInches * totalInches;
-		
-		return totalInches2;
-	}
-	
-	//BMI Calculation [NOT WORKING!!!]
-	public static double bmiCalc(int weight, int height) {
-		double bmi = weight / height;
-		
-		return bmi;
-		
 	}
 	
 }
